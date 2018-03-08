0675d8f5c310ebfc8ed5721974585877a999e594
Olle Kaiser <yogsther@gmail.com>
2018-03-08 02:04:12 +0100

    Update

--- items.js
+++ items.js
@@ -1,18 +1,4 @@
 var items = [{
-  name: "Silver Pocket Expander 10",
-  rarity: 0,
-  marketable: true,
-  code: "pocket_10",
-  useable: true,
-  description: "Expand your silver-bag with 10 extra slots!"
-}, {
-  name: "Silver Pocket Expander 50",
-  rarity: 1,
-  marketable: true,
-  code: "pocket_50",
-  useable: true,
-  description: "Expand your silver-bag with 50 extra slots!"
-}, {
   name: "Golden Watch",
   rarity: 4,
   marketable: true,
@@ -24,7 +10,7 @@ var items = [{
   rarity: 0,
   code: "crate_0",
   price: 50,
-  description: "Limited crates that will only be avalible during the Beta.",
+  description: "Limited crates that are only be avalible for purchase during the Beta.",
   useable: true,
   marketable: true,
   use: "openCrate(0)",
@@ -44,4 +30,83 @@ var items = [{
   marketable: true,
   useable: false,
   description: "Mysterious black box.. It's heavy too."
+},{
+  name: "Dino and Martin",
+  rarity: 1,
+  marketable: true,
+  code: "dino_martin",
+  useable: false,
+  description: "Bootleg merchandise from China. Bump and Go!"
+},{
+  name: "Red Flair",
+  rarity: 0,
+  marketable: true,
+  code: "red_flair",
+  useable: false,
+  description: "Bootleg merchandise from China. Bump and Go!"
+}, {
+  name: "The Bird",
+  rarity: 3,
+  marketable: true,
+  code: "the_bird",
+  useable: false,
+  description: "'Do you have the bird?' 🤔"
+}, {
+  name: "😂",
+  rarity: 4,
+  marketable: true,
+  code: "the_joy",
+  useable: false,
+  description: "Very rare emoji"
+}, {
+  name: "Rare Pepe",
+  rarity: 4,
+  marketable: true,
+  code: "rare_pepe",
+  useable: false,
+  description: "The rarest pepe you will find on the market..."
+}, {
+  name: "Black Sunglasses",
+  rarity: 0,
+  marketable: true,
+  code: "sunglasses",
+  useable: false,
+  description: "Ordinary slick looking sunglasses."
+}, {
+  name: "Starbucks Cup",
+  rarity: 1,
+  marketable: true,
+  code: "starbucks",
+  useable: false,
+  description: "Is it half-empty or half-full?."
+}, {
+  name: "Silver Key",
+  rarity: 2,
+  marketable: true,
+  code: "silver_key",
+  useable: false,
+  description: "What does it lead too?"
+}, {
+  name: "Cursed Bible",
+  rarity: 3,
+  marketable: true,
+  code: "bible",
+  useable: false,
+  description: "Almost falling apart, tied together with a rosehip branch."
+}, {
+  name: "Original Minecraft (PE) copy",
+  rarity: 1,
+  marketable: true,
+  code: "mcpe",
+  useable: false,
+  description: "A copy of the popular game Minecraft Pocket Edition for IOS, worth $5."
+}, {
+  name: "USSR Launch Keys",
+  rarity: 1,
+  marketable: true,
+  code: "launch_keys",
+  useable: false,
+  description: "Launch keys used by the USSR to launch their nuclear weapons."
 }];
+
+
--- stock.js
+++ stock.js
@@ -1,15 +1,15 @@
-
 /**
  * Stock Farmer - v.0.2
  * Client Side Main Script
  * Yogsther 2018
  */
 
- /* Import socket */
+/* Import socket */
 //var socket = io.connect("213.66.254.63:25565"); TODO UNCOMMENT
 
 var silver = 0;
-var backpackSize = 50;
+
+var backgroundColors = ["#384459", "#633434", "#34634e", "#63344d"];
 
 const rareities = ["#dddddd", "#70d177", "#70c0d1", "#e86fb5", "#f7d259"];
 
@@ -55,41 +55,55 @@ texture_arrow_two.src = "textures/arrow_two.png";
 var texture_arrow_three = new Image();
 texture_arrow_three.src = "textures/arrow_three.png";
 
+var texture_mainBorder = new Image();
+texture_mainBorder.src = "textures/main-border.png";
+
+var texture_statusbar = new Image();
+texture_statusbar.src = "textures/status-bar.png";
+
+/* Download sounds */
+
+var sound_gas = new Audio();
+sound_gas.src = "sound/gas.mp3"
+
+var sound_tier_one = new Audio();
+sound_tier_one.src = "sound/tier_one.mp3"
+
+var sound_tier_two = new Audio();
+sound_tier_two.src = "sound/tier_two.mp3"
+
+
 var gemsToSort = new Array();
-
 var gems = ["red", "blue", "green"];
-
 var mouseDown = false;
-
 var picked = false;
+var mousePos = {
+  x: 0,
+  y: 0
+};
+var multiplierStatus = 0;
 
-var mousePos = {x: 0, y: 0};
-
-canvas.addEventListener("mousedown", function(e){
-  console.log(mousePos)
-   mouseDown = true;
-   if(mousePos.x > 204.5 && mousePos.x < 257.5 && mousePos.y > 39 && mousePos.y < 97){
+canvas.addEventListener("mousedown", function (e) {
+  mouseDown = true;
+  if (mousePos.x > 307.5 && mousePos.x < 354.5 && mousePos.y > 39 && mousePos.y < 97) {
     pickUp();
-   }
+  }
 });
 
-canvas.addEventListener("mouseup", function(e){
+canvas.addEventListener("mouseup", function (e) {
   mouseDown = false;
-  if(picked){
+  if (picked) {
     drop();
   }
 });
 
-canvas.addEventListener("mousemove", function(e){
+canvas.addEventListener("mousemove", function (e) {
   var rect = canvas.getBoundingClientRect();
   mousePos.x = e.clientX - rect.left;
   mousePos.y = e.clientY - rect.top;
-
-  
-
 });
 
-function pickUp(){
+function pickUp() {
   picked = true;
 
   activeGem.active = true;
@@ -98,7 +112,7 @@ function pickUp(){
   activeGem.y = gemsToSort[0].y;
 
   gemsToSort.splice(0, 1);
-  spawnGem(gemsToSort[gemsToSort.length-1].x - 50, 50);
+  spawnGem(gemsToSort[gemsToSort.length - 1].x - 50, 50);
 }
 
 var keysDown = new Array();
@@ -106,54 +120,68 @@ var keysDown = new Array();
 var keycodes = [37, 38, 39];
 var colorOrder = ["red", "green", "blue"]
 
-document.addEventListener("keydown", function(e){
+document.addEventListener("keydown", function (e) {
   keysDown.push(e.keyCode);
 
-  for(let i = 0; i < keycodes.length; i++){
-    if(keycodes[i] == e.keyCode){
+  for (let i = 0; i < keycodes.length; i++) {
+    if (keycodes[i] == e.keyCode) {
       pickUp();
       picked = false;
       activeGem.active = false;
-      if(colorOrder[i] == activeGem.name){
+      if (colorOrder[i] == activeGem.name) {
         giveSilver(1);
         oneUpEffect(activeGem.name)
+        successDrop();
+      } else {
+        failedDrop();
       }
     }
-  }  
+  }
 });
 
-document.addEventListener("keyup", function(e){
+document.addEventListener("keyup", function (e) {
   remove();
-  function remove(){
-    for(let i = 0; i < keysDown.length; i++){
-      if(keysDown[i] == e.keyCode) keysDown.splice(i, 1);
+
+  function remove() {
+    for (let i = 0; i < keysDown.length; i++) {
+      if (keysDown[i] == e.keyCode) keysDown.splice(i, 1);
     }
-    if(keyDown(e.keyCode)) remove();
+    if (keyDown(e.keyCode)) remove();
   }
-  
-  
+
+
 })
 
-function drop(){
+function drop() {
   picked = false;
   activeGem.active = false;
 
-  if(mousePos.y > 132){
+  if (mousePos.y > 132) {
     // Some option
     var pick = "red";
-    if(mousePos.x > 191) pick = "green";
-    if(mousePos.x > 444.5) pick = "blue";
+    if (mousePos.x > 191) pick = "green";
+    if (mousePos.x > 444.5) pick = "blue";
 
-    if(pick == activeGem.name){
+    if (pick == activeGem.name) {
       giveSilver(1);
       oneUpEffect(pick);
-    } 
+      successDrop();
+      return;
+    }
   }
-
-
+  failedDrop();
 }
 
-for(let i = 0; i < 5; i++){
+function successDrop() {
+  multiplierStatus += 0.1;
+}
+
+function failedDrop() {
+  multiplierStatus = 0;
+  giveSilver(-1)
+}
+
+for (let i = 0; i < 8; i++) {
   spawnGem(210 - (i * 50), 50);
 }
 
@@ -164,9 +192,9 @@ var activeGem = {
   name: ""
 }
 
-function spawnGem(x, y){
+function spawnGem(x, y) {
   gemsToSort.push({
-    name: gems[Math.floor(Math.random()*gems.length)],
+    name: gems[Math.floor(Math.random() * gems.length)],
     x: x,
     y: y,
     active: false
@@ -175,11 +203,11 @@ function spawnGem(x, y){
 
 var mouseOverCanvas = false;
 
-canvas.addEventListener("mouseover", function(e){
+canvas.addEventListener("mouseover", function (e) {
   mouseOverCanvas = true;
 })
 
-canvas.addEventListener("mouseout", function(e){
+canvas.addEventListener("mouseout", function (e) {
   mouseOverCanvas = false;
 })
 
@@ -195,16 +223,16 @@ var blockColors = ["#ff314d", "#2af772", "#00b9ff"];
 /* Stores the current one up texts */
 var activeOneUps = new Array();
 
-function oneUpEffect(inputColor){
+function oneUpEffect(inputColor) {
   /* Red or undefined */
   var boundingBox = [38.5, 209.5];
   var color = blockColors[0];
-  if(inputColor == "green"){
+  if (inputColor == "green") {
     /* For color green */
-    
+
     boundingBox = [248.5, 418.5];
     color = blockColors[1];
-  } else if(inputColor == "blue"){
+  } else if (inputColor == "blue") {
     /* For color blue */
     boundingBox = [459.5, 629.5];
     color = blockColors[2];
@@ -212,7 +240,7 @@ function oneUpEffect(inputColor){
   spawnText(color, boundingBox[0], boundingBox[1]);
 }
 
-function spawnText(color, x1, x2){
+function spawnText(color, x1, x2) {
   var xPosition = Math.floor(Math.random() * (x2 - x1)) + x1;
   activeOneUps.push({
     x: xPosition,
@@ -225,72 +253,64 @@ function spawnText(color, x1, x2){
 
 
 
-render();
+var lastStage = 0;
 
-function keyDown(keycode){
-  for(let i = 0; i < keysDown.length; i++){
-    if(keysDown[i] == keycode) return true;
+function keyDown(keycode) {
+  for (let i = 0; i < keysDown.length; i++) {
+    if (keysDown[i] == keycode) return true;
   }
   return false;
 }
 
 
+/* TODO: DO SOMETHINFG ABOUT THIS */
+var todaysColor = backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
 
-function render(){
+var lastDisplayedStatusBar = 0;
 
-  if(gemsToSort[0].x < 210){
-    for(let i = 0; i < gemsToSort.length; i++){
-      gemsToSort[i].x+=10;
+function render() {
+
+  if (gemsToSort[0].x < 308) {
+    for (let i = 0; i < gemsToSort.length; i++) {
+      gemsToSort[i].x += 10;
     }
   }
 
   var arrowCodes = ["one", "two", "three"];
-  
-  for(let i = 0; i < keycodes.length; i++){
-    if(keyDown(keycodes[i])){
-      eval("arrowHeights." +  arrowCodes[i] + " = -10") 
+
+  for (let i = 0; i < keycodes.length; i++) {
+    if (keyDown(keycodes[i])) {
+      eval("arrowHeights." + arrowCodes[i] + " = -10")
     } else {
-      eval("arrowHeights." +  arrowCodes[i] + " = 0") 
+      eval("arrowHeights." + arrowCodes[i] + " = 0")
     }
   }
 
 
+  if (multiplierStatus < 0) multiplierStatus = 0;
+
+
+  ctx.fillStyle = todaysColor;
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(texture_bg, 0, 0); /* Draw background */
-  if(!mouseOverCanvas){
-    ctx.globalCompositeOperation='none';
+  if (!mouseOverCanvas) {
+    ctx.globalCompositeOperation = 'none';
     ctx.drawImage(texture_arrow_one, 0, arrowHeights.one);
     ctx.drawImage(texture_arrow_two, 0, arrowHeights.two);
     ctx.drawImage(texture_arrow_three, 0, arrowHeights.three);
   }
 
-  for(let i = 0; i < activeOneUps.length; i++){
-
-    /* Move the one up */
-    activeOneUps[i].opacity -= .02;
-    activeOneUps[i].y -= 1.2;
-    if(activeOneUps[i].opacity < 0){
-        activeOneUps.splice(i, 1); /* Remove from array if dead. */
-      } else { 
-      /* Render one up */
-      ctx.fillStyle = activeOneUps[i].color;
-      ctx.globalAlpha = activeOneUps[i].opacity;
-      ctx.font = "20px Ubuntu";
-      ctx.fillText("+1", activeOneUps[i].x, activeOneUps[i].y);
-    }
-  }
-  
-  ctx.globalAlpha = 1;
-
   /* Render travelators */
   ctx.drawImage(texture_band, 0, 0);
-  ctx.drawImage(texture_band, 70, 0);
+  ctx.drawImage(texture_band, 80, 0);
+  ctx.drawImage(texture_band, 180, 0);
 
   /* Render all active gems */
-  for(let i = 0; i < gemsToSort.length; i++){
+  for (let i = 0; i < gemsToSort.length; i++) {
     ctx.drawImage(eval("texture_" + gemsToSort[i].name), gemsToSort[i].x, gemsToSort[i].y);
   }
 
-  if(activeGem.active){
+  if (activeGem.active) {
     ctx.drawImage(eval("texture_" + activeGem.name), mousePos.x - 20, mousePos.y - 20);
   }
 
@@ -302,6 +322,99 @@ function render(){
   /* Render outline */
   ctx.drawImage(texture_outline, 0, 0);
 
+  /* Draw multiplier status bar */
+  ctx.fillStyle = "grey";
+  ctx.fillRect(607.5, 67, 60, 50);
+
+
+
+  var multiplierColor = "#fff"
+  window.multiplier = 1;
+  multiplierPercent = multiplierStatus * 100;
+  multiplierStatus -= 0.002 * (multiplierPercent / 30);
+
+
+  if (multiplierPercent > 100) multiplierPercent = 100; /* Lock */
+
+  var stages = [{
+    multiplier: 1,
+    color: "#fff",
+    percent: 0
+  }, {
+    multiplier: 2,
+    color: "#3ca2fc",
+    percent: 32,
+    sound: sound_tier_one
+  }, {
+    multiplier: 5,
+    color: "#f93953",
+    percent: 55,
+    sound: sound_tier_two
+  }, {
+    multiplier: 15,
+    color: "#ffc832",
+    percent: 75,
+    sound: sound_gas
+  }, {
+    multiplier: 1,
+    color: "fallback",
+    percent: 9999
+  }]
+
+
+  for(let i = 0; i < stages.length; i++){
+    if(multiplierPercent > stages[i].percent && multiplierPercent < stages[i+1].percent){
+      multiplierColor = stages[i].color;
+      multiplier = stages[i].multiplier;
+      if(lastStage < i){
+        if(stages[i].sound.paused){
+        stages[i].sound.volume = 0.5;
+        stages[i].sound.currentTime = 0;
+        stages[i].sound.play();
+      }
+      } else if(lastStage > i && lastStage == 3){
+        stages[i+1].sound.pause();
+      }
+      lastStage = i;
+      break;
+    }
+  }
+
+  var lastMultiplier = multiplier;
+  var statusWidth = 230 * (multiplierPercent * 0.01);
+  
+  if(statusWidth > lastDisplayedStatusBar) lastDisplayedStatusBar += 5;
+  if(statusWidth < lastDisplayedStatusBar) lastDisplayedStatusBar = statusWidth;
+
+  /* Render status bar */
+  ctx.fillRect(430, 26, 244, 30);
+  ctx.fillStyle = multiplierColor;
+  ctx.fillRect(660 - lastDisplayedStatusBar, 26, 244, 30)
+
+  ctx.font = "20px Ubuntu";
+  ctx.fillText(multiplier + "x", 622, 98);
+
+  ctx.drawImage(texture_statusbar, 0, 0)
+
+  for (let i = 0; i < activeOneUps.length; i++) {
+    /* Move the one up */
+    activeOneUps[i].opacity -= .02;
+    activeOneUps[i].y -= 1.2;
+    if (activeOneUps[i].opacity < 0) {
+      activeOneUps.splice(i, 1); /* Remove from array if dead. */
+    } else {
+      /* Render one up */
+      ctx.fillStyle = activeOneUps[i].color;
+      ctx.globalAlpha = activeOneUps[i].opacity;
+      ctx.font = "20px Ubuntu";
+      ctx.fillText("+" + (1 * multiplier), activeOneUps[i].x, activeOneUps[i].y);
+    }
+  }
+  ctx.globalAlpha = 1;
+
+  /* Render out black border */
+  ctx.drawImage(texture_mainBorder, 0, 0)
+
   /* Request callback */
   requestAnimationFrame(render);
 }
@@ -312,17 +425,18 @@ window.onload = function () {
   for (let i = 0; i < items.length; i++) {
     inventory[i] = 10;
   }
+  render();
   //loadPrePage();
 }
 
-function loadPrePage(){
+function loadPrePage() {
   /* Load login and signup page */
   document.getElementById("overlay").innerHTML = '<div id="overlay-block"> <div id="signup-page"> <img src="logo.png" id="signup-logo"> <span id="welcome-text">Welcome, please sign up or login to play!</span><br> <input id="username" class="login-input" oninput="checkUsername()" placeholder="Username" title="This will be your Display name and login-name! Only A-Z and Numbers allowed!" oninput="checkUsername()"> <input id="pin" title="(Must be numbers) Pin can be between 4-20 numbers long, this is your password." class="login-input" placeholder="Pin" type="password" oninput="checkUsername()"> <span id="signerbuttons"><button class="btn signup-button" id="signup-button" disabled>Sign up!</button> <button class="btn signup-button" id="login-button" disabled>Log in!</button></span><span id="error_login"></span> </div> </div>';
 }
 
-function checkUsername(){
+function checkUsername() {
 
-  if(document.getElementById("username").value == "" || document.getElementById("pin").value == ""){
+  if (document.getElementById("username").value == "" || document.getElementById("pin").value == "") {
     document.getElementById("signup-button").disabled = true;
     document.getElementById("login-button").disabled = true;
     return;
@@ -347,7 +461,7 @@ function checkUsername(){
 
 }); */
 
-function errorLoginMessage(message){
+function errorLoginMessage(message) {
   document.getElementById("error_login").innerHTML = message;
 }
 
@@ -360,10 +474,8 @@ function getCrateByID(id) {
 }
 
 
-
 function giveSilver(amount) {
-  silver += amount;
-  if (silver > backpackSize) silver = backpackSize;
+  silver += amount * multiplier;
   document.getElementById("silver-value").style.fontSize = "40px";
   setTimeout(function () {
     document.getElementById("silver-value").style.fontSize = "30px";
@@ -383,7 +495,9 @@ function use() {
   if (item.useable) eval(item.use);
 }
 
-var crateContains = [["pocket_10", "pocket_50", "gold_watch", "orange_juice_strong", "black_box"]];
+var crateContains = [
+  ["gold_watch", "orange_juice_strong", "black_box", "dino_martin", "red_flair", "the_bird", "the_joy", "rare_pepe", "sunglasses", "starbucks", "silver_key", "bible", "mcpe", "launch_keys"]
+];
 
 function itemFromCode(code) {
   /* Code is the keyword or id for that item, ex. gold_watch */
@@ -392,8 +506,6 @@ function itemFromCode(code) {
   }
   return false;
 }
-
-
 
 function randomBoolean() {
   if (Math.random() > 0.5) return true;
@@ -420,7 +532,7 @@ function openCrate(id) {
     if (rarityNum > 60) rarity = 1;
     if (rarityNum > 85) rarity = 2;
     if (rarityNum > 95) rarity = 3;
-    if (rarityNum > 99) rarity = 4;
+    if (rarityNum > 98) rarity = 4;
 
     var possibleItems = new Array();
     for (let i = 0; i < contents.length; i++) {
@@ -476,9 +588,22 @@ function encryptText(text) {
 function inspect(id) {
   /* Inspect item */
   var item = items[id];
+
   window.currentInspect = id;
   document.getElementById("item-name").innerHTML = "<span style='color:" + rareities[item.rarity] + "'>" + item.name + "</span>";
   document.getElementById("item-description").innerHTML = item.description;
+
+  if (item.crateID != undefined) {
+    /* Item is a crate, display crate contents. */
+    var itemsArray = getCrateItemsAsArray(item.crateID);
+
+    document.getElementById("item-description").innerHTML += "<br><br>Crate contains: "
+    for (let i = 0; i < itemsArray.length; i++) {
+      var displayItem = itemsArray[i]
+      document.getElementById("item-description").innerHTML += "<br><span style='color:" + rareities[displayItem.rarity] + ";'>" + displayItem.name + "</span>"
+    }
+
+  }
 
   document.getElementById("use-button").disabled = !item.useable;
   document.getElementById("quicklist-button").disabled = !item.marketable;
@@ -486,6 +611,18 @@ function inspect(id) {
 
 function openCrateStore() {
   //document.getElementById("overlay").innerHTML
+}
+
+function getCrateItemsAsArray(crateID) {
+  var contents = crateContains[crateID];
+  var itemsArray = new Array();
+  for (let i = 0; i < contents.length; i++) {
+    itemsArray.push(itemFromCode(contents[i]));
+  }
+  itemsArray.sort(function (a, b) {
+    return b.rarity - a.rarity;
+  });
+  return itemsArray;
 }
 
 function listItems() {
